62becd16ff7c5cb5f0a7f700ce938130acd0e3ab
kateoneill <20088535@mail.wit.ie>
2022-10-05 16:14:27 +0100

    Add Junit testing for duplicates, negative numbers and one number

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -8,4 +8,19 @@ class MainTest {
         assertEquals(9, findLargest(intArrayOf(8, 9, 7)))
         assertEquals(9, findLargest(intArrayOf(7, 8, 9)))
     }
+
+    @Test
+    fun testDuplicates() {
+        assertEquals(9, findLargest(intArrayOf(9, 7, 9, 8)))
+    }
+
+    @Test
+    fun testForOnlyOneNumber() {
+        assertEquals(7, findLargest(intArrayOf(7)))
+    }
+
+    @Test
+    fun testNegativeNumbers(){
+        assertEquals(-7, findLargest(intArrayOf(-7, -8, -9)))
+    }
 }
